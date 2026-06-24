6431365a743a8cfb6b3f7f6a2ca67040de6a0f15
Daniel Rangel <danranngel@gmail.com>
2026-06-24 08:55:52 +0200

    sets max width

--- web/src/components/MainLayout/Footer/Footer.jsx
+++ web/src/components/MainLayout/Footer/Footer.jsx
@@ -5,12 +5,14 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className='app-footer-56yl'>
-      <div className='app-footer-56yl__container'>
-        <p className='app-footer-56yl__text'>
-          🍇 {currentYear} Oovah.
-        </p>
-      </div>
-    </footer>
+    <></>
+    // remove for now
+    // <footer className='app-footer-56yl'>
+    //   <div className='app-footer-56yl__container'>
+    //     <p className='app-footer-56yl__text'>
+    //       🍇 {currentYear} Oovah.
+    //     </p>
+    //   </div>
+    // </footer>
   );
 };
